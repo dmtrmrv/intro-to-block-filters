59eb6a428ff473a87ddc1b5d86476e44ae8723be
Dmitry Mayorov <iamdmitrymayorov@gmail.com>
2020-11-09 20:08:25 -0500

    Initial commit

---
+++ button-arrows.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Button Arrows
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     button-arrows
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Button_Arrows
+ */
+
+// Your code starts here.
